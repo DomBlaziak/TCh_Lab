fd325d1995c02516d64729b8245ffb2e8b01d37b
DomBlaziak <blaziakdominik@gmail.com>
2026-04-22 20:00:57 +0200

    Poprawki

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,10 @@ var config = require('../config');
 
 // Predefiniowana lista miast
 const cities = [
-    { name: "Warsaw", query: "Warsaw" },
-    { name: "Berlin", query: "Berlin" },
-    { name: "London", query: "London" },
-    { name: "Rome", query: "Rome" }
+    { name: "Warsaw", query: "Warsaw,PL" },
+    { name: "Berlin", query: "Berlin,DE" },
+    { name: "London", query: "London,GB" },
+    { name: "Rome", query: "Rome,IT" } 
 ];
 
 router.get('/', function(req, res) {
